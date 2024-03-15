4c4cb44c23fcbfc80d0e210fd18a0127e06fb73b
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-15 10:41:51 -0500

    completed the add notes functionality and the delete notes functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const currentYear = new Date().getFullYear();
-    return(<footer><p>Copywrite ⓒ {currentYear}</p></footer>);
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
